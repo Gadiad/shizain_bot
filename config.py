61dc5dcc70dcfca370b78cb854cd2def917d8839
Gadiad <117043325+Gadiad@users.noreply.github.com>
2022-11-02 00:55:12 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "5714150030:AAGatoCz_E_CryJVeYhtSP4XEGs33hr15OY"
+APP_URL = "https://shizain.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://puituzgmngqiwh:9b4f079c0766513bd92150923a63bb17c1d3505460b059396fcb8d0599757aa2@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/dcrqes2g6amhmi"
